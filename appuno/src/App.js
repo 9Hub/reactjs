4d0dc944d4312d3d63bf053e75e227c1e081dd8b
edwinpaye <baratheonts@gmail.com>
2019-01-16 23:54:13 -0400

    add compoents App

--- appuno/src/App.js
+++ appuno/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Boton from './components/Boton';
+import Titulo from './components/Titulo';
+// import './App.css';
 
 class App extends Component {
+
+  nombre = "hola";
+
+  // changeName=()=>{
+  //   this.nombre = "funciona"
+  //   this.Boton.texto = "f"
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Titulo titulo='Web Site'></Titulo>
+        <Boton titulo='menu'></Boton>
+        {/* <button className="boton" onClick={this.changeName}>Menu3</button> */}
+        {/* <Imagen direccion="hola"></Imagen> */}
       </div>
     );
   }
